407ca6831195504fdf5e4a7994b95205ab3015fc
nevadimka <myregbox@outlook.com>
2022-01-15 18:28:45 +0300

    Crypto bot example.

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = '5014720599:AAF0f6HnLgUymhRfj-4QKpm5JuTgi63aB-c'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+}
--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import telebot
+from config import keys, TOKEN
+from utils import CryptoConverter, ConversionException
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+bot = telebot.TeleBot(TOKEN)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@bot.message_handler(commands=['start', 'help'])
+def help_command(message: telebot.types.Message):
+    text = 'Чтобы начать работу введите команду боту в следующем формате:\n<имя валюты>' \
+           '<в какую валюту перевести> <количество переводимой валюты>' \
+           '\nУвидить список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def values_command(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+        if len(values) != 3:
+            raise ConversionException('Too many parameters!')
+        quote, base, amount = values
+        total_base = CryptoConverter.convert(quote, base, amount)
+    except ConversionException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+    else:
+        text = f"Цена {amount} {quote} в {base} - {total_base}"
+        bot.send_message(message.chat.id, text)
+
+
+bot.polling()
